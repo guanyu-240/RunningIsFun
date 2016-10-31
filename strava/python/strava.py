b314ce9a9a55f93c106d34d3ea04f88b70fc82f5
Predrag Wang <guanyuwang.cs@gmail.com>
2016-10-31 08:38:30 -0400

    queries added

--- strava/python/strava.py
+++ strava/python/strava.py
@@ -10,6 +10,7 @@ import os
 import ConfigParser
 import urllib
 import json
+from datetime import date,timedelta,datetime
 
 # from pypi
 import requests
@@ -20,8 +21,22 @@ ATHLETE_URL = "https://www.strava.com/api/v3/athlete"
 ATHLETES_URL = "https://www.strava.com/api/v3/athletes/{}"
 ACTIVITIES_URL = "https://www.strava.com/api/v3/activities/{}"
 ATHLETE_ACTIVITIES_URL = "https://www.strava.com/api/v3/athlete/activities"
-CLUBS_URL = "https://www.strava.com/api/v3/clubs/{}"
+CLUBS_URL = "https://www.strava.com/api/v3/clubs/{}{}"
 
+# functions
+def get_start_of_week():
+  """
+  Get the start of current week
+  """
+  today = date.today()
+  week_start = today - timedelta(today.weekday())
+  return week_start
+
+def convert_datestr(date_str):
+  """
+  Format the datetime string
+  """
+  return datetime.strptime(date_str, '%Y-%m-%dT%H:%M:%SZ')
 
 ################################################################################
 class Strava():
@@ -86,8 +101,32 @@ class Strava():
     Get the information of a club
     """
     params = {'access_token': self.__accessToken}
-    r =  requests.get(CLUBS_URL.format(club_id), params)
+    r =  requests.get(CLUBS_URL.format(club_id, ''), params)
     r.raise_for_status()
     return r.json()
 
+  def getClubActivities(self, club_id, before=None, page=None, per_page=None):
+    """
+    Get the list of club activities
+    """
+    params = {'access_token': self.__accessToken}
+    if before: params['before'] = before
+    if page: params['page'] = page
+    if per_page: params['per_page'] = per_page
+    r =  requests.get(CLUBS_URL.format(club_id, '/activities'), params)
+    r.raise_for_status()
+    return r.json()
 
+  """
+  Commonly used complex queries
+  """
+  def getClubActivitiesCurWeek(self, club_id):
+    """
+    Get the list of club activities in the current week
+    """
+    club_activities = self.getClubActivities(club_id)
+    week_start = get_start_of_week()
+    club_activities_curweek = filter( \
+            lambda x: convert_datestr(x['start_date']).date() >= week_start, \
+            club_activities)
+    return club_activities_curweek
