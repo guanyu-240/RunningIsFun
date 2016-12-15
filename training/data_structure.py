bedb23fabc78a7054fefc92fd0b0e207d3d565f9
Guanyu Wang <guanyuwang.cs@gmail.com>
2016-12-15 17:42:45 -0500

    Update

--- training/data_structure.py
+++ training/data_structure.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python
 
+"""
+Data structures
+"""
+################################################################################
+
 MARATHON_KM = 42.195
 MARATHON_MI = 26.2188
 
@@ -7,6 +12,13 @@ def pace_str(seconds):
   return "{}:{:02d}".format(seconds/60, seconds%60)
 
 class TrainingPace:
+  """
+  Training pace class
+  training: str, training name
+  pace1: int, training pace in seconds
+  pace2: int, training pace in seconds, 
+         the max pace if the training pace is in a range
+  """
   def __init__(self, training, pace1, pace2):
     self.training = training
     self.pace1 = pace1
